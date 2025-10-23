cfabcf6c7d30053d92f5aec3b3c373e8124e5c52
Shanker Singh <shanker2605@gmail.com>
2025-10-23 15:53:00 +0530

    lesson: beforeEach

---
+++ cypress/e2e/before-each.cy.js
@@ -0,0 +1,29 @@
+describe('Max character input functionality', () => {
+
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/example-3')
+
+        cy.get('span[data-cy=first-name-chars-left-count]').as('fnCharsLeft')
+        cy.get('#first-name-input').as('fnInput')
+    })
+
+  it('displays the appropriate remaining characters count', () => {
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '15')
+    cy.get('@fnInput').type('Hello')
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '10')
+  })
+
+   it('displays the appropriate remaining characters count', () => {
+
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '15')
+    cy.get('@fnInput').type('HelloHello')
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '5')
+  })
+
+   it('displays the appropriate remaining characters count', () => {
+    
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '15')
+    cy.get('@fnInput').type('HelloHelloHelloHello')
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '0')
+  })
+})
