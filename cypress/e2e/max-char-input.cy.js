a294bafb74367d2ac635f5f075cac6aa2554a737
Shanker Singh <shanker2605@gmail.com>
2025-10-23 12:26:26 +0530

    second test

---
+++ cypress/e2e/max-char-input.cy.js
@@ -0,0 +1,8 @@
+describe('Heading Text', () => {
+  it('contains title of our site', () => {
+    cy.visit('http://localhost:5173/example-2')
+    cy.get('span').invoke('text').should('equal', '15')
+    cy.get('input').type('Hello')
+    cy.get('span').invoke('text').should('equal', '10')
+  })
+})
