1c93f8df064a244aff7360900c2abece4c687046
Shanker Singh <shanker2605@gmail.com>
2025-10-23 12:53:08 +0530

    lesson: alias

--- cypress/e2e/selecting.cy.js
+++ cypress/e2e/selecting.cy.js
@@ -1,8 +1,10 @@
 describe('Max character input functionality', () => {
   it('displays the appropriate remaining characters count', () => {
     cy.visit('http://localhost:5173/example-3')
-    cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').should('equal', '15')
-    cy.get('#first-name-input').type('Hello')
-    cy.get('span[data-cy=first-name-chars-left-count]').invoke('text').should('equal', '10')
+    cy.get('span[data-cy=first-name-chars-left-count]').as('fnCharsLeft')
+    cy.get('#first-name-input').as('fnInput')
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '15')
+    cy.get('@fnInput').type('Hello')
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '10')
   })
 })
