5e17f33cbcbe9d29307733d0cd87235ba2f0d28b
Shanker Singh <shanker2605@gmail.com>
2025-10-23 15:47:13 +0530

    lesson: .then() callback

---
+++ cypress/e2e/results.cy.js
@@ -0,0 +1,22 @@
+describe('Max character input functionality', () => {
+  it('displays the appropriate remaining characters count', () => {
+    cy.visit('http://localhost:5173/example-3')
+
+    cy.get('span[data-cy=first-name-chars-left-count]').then(function($charsLeftSpan){
+      expect($charsLeftSpan.text()).to.equal('15')
+    })
+
+    // Alias the elements for easier reference
+    cy.get('span[data-cy=first-name-chars-left-count]').as('fnCharsLeft')
+    cy.get('#first-name-input').as('fnInput')
+    
+    // Initial assertion
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '15')
+    // Type into the input and assert the remaining characters
+    cy.get('@fnInput').type('Hello')
+    // Final assertions
+    cy.get('@fnCharsLeft').invoke('text').should('equal', '10')
+    // Verify the input value
+    cy.get('@fnInput').should('have.value', 'Hello')
+  })
+})
